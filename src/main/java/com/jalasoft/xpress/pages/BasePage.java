c9d1ebe48f8a579261fd92b3f54a168e32ec995f
MijhailVillarroel <Mijhail.Villarroel@fundacion-jala.org>
2016-09-01 19:04:18 -0400

    add login page

--- src/main/java/com/jalasoft/xpress/pages/BasePage.java
+++ src/main/java/com/jalasoft/xpress/pages/BasePage.java
@@ -1,7 +1,32 @@
 package com.jalasoft.xpress.pages;
 
+import com.jalasoft.xpress.framework.selenium.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static com.jalasoft.xpress.framework.util.Constants.WAIT_TIME;
+
 /**
  * Created by Mijhail Villarroel on 9/1/2016.
  */
-public class BasePage {
+public abstract class BasePage {
+
+    protected WebDriver driver;
+
+    protected WebDriverWait wait;
+
+    protected Actions action;
+
+    public BasePage() {
+        driver = DriverManager.getInstance().getDriver();
+        wait = new WebDriverWait(driver, WAIT_TIME);
+        action = new Actions(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void refreshPage() {
+        driver.navigate().refresh();
+    }
 }
--- src/test/java/com/jalasoft/express/cucumber/stepdefinition/login/LoginStepDef.java
+++ src/test/java/com/jalasoft/express/cucumber/stepdefinition/login/LoginStepDef.java
@@ -1,7 +1,22 @@
-package PACKAGE_NAME;
+package com.jalasoft.express.cucumber.stepdefinition.login;
+
+import com.jalasoft.xpress.pages.Login;
+import com.jalasoft.xpress.pages.ProsHome;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
 
 /**
  * Created by Mijhail Villarroel on 9/1/2016.
  */
 public class LoginStepDef {
+    @Given("^I login with valid credentials$")
+    public void iLoginWithValidCredentials() throws Throwable {
+        ProsHome prosHome = new ProsHome();
+        Login login = prosHome.clickSingInLink();
+        login.clickButtonInternalUser();
+        login.setUserNameTestField("mijhailV");
+        login.setPasswordTestField("admin123P");
+        login.clickSignInButton();
+
+    }
 }
