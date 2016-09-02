c781ee0ab14c1eb762711464067733aa2aa4dc60
MijhailVillarroel <Mijhail.Villarroel@fundacion-jala.org>
2016-09-02 12:23:08 -0400

    add hook for delete projects

--- src/test/java/com/jalasoft/express/cucumber/hooks/Projects.java
+++ src/test/java/com/jalasoft/express/cucumber/hooks/Projects.java
@@ -1,7 +1,19 @@
 package com.jalasoft.express.cucumber.hooks;
 
+import cucumber.api.java.After;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Mijhail Villarroel on 9/2/2016.
  */
 public class Projects {
+
+    /**
+     * This method hook is used after a project is created using selenium.
+     */
+    @After("@ProjectSelenium")
+    public void tearDown() {
+       
+    }
 }
