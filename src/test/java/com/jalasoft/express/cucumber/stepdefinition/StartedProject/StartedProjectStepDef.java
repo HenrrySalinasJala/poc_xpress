a46650540266bb5da2c78e89169fec916b72b256
DanielRGonzalesV <drgvandroid@gmail.com>
2016-09-06 16:18:37 -0400

    Fix properties

--- src/test/java/com/jalasoft/express/cucumber/stepdefinition/StartedProject/StartedProjectStepDef.java
+++ src/test/java/com/jalasoft/express/cucumber/stepdefinition/StartedProject/StartedProjectStepDef.java
@@ -3,20 +3,13 @@ package com.jalasoft.express.cucumber.stepdefinition.StartedProject;
 import com.jalasoft.express.cucumber.stepdefinition.login.LoginStepDef;
 import com.jalasoft.express.cucumber.stepdefinition.project.ProjectStepDef;
 import com.jalasoft.xpress.pages.Dashboard;
-import com.jalasoft.xpress.pages.Mapper;
-import com.jalasoft.xpress.pages.Menu;
 import com.jalasoft.xpress.pages.Projects;
 import com.jalasoft.xpress.pages.StartedProjects.Dimensions;
-import com.jalasoft.xpress.pages.StartedProjects.FromExistingProject;
 import com.jalasoft.xpress.pages.StartedProjects.FromScenarios;
 import com.jalasoft.xpress.pages.StartedProjects.SetupDashboard;
-import com.jalasoft.xpress.pages.project.ProjectSteps;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
-
-import java.util.Map;
 
 import static com.jalasoft.xpress.pages.Mapper.addProjects;
 import static com.jalasoft.xpress.pages.Mapper.getValueProject;
@@ -28,16 +21,12 @@ import static com.jalasoft.xpress.pages.Mapper.getValueProject;
 public class StartedProjectStepDef {
 
     private ProjectStepDef projectStepDef;
-    LoginStepDef loginStepDef;
-    Dashboard dashboard;
-    Projects projects;
-    SetupDashboard setupDashboard;
-    FromExistingProject fromExistingProject;
-    FromScenarios fromScenarios;
-    Dimensions dimensions;
-    Map<String, Map> valuesProject;
-    String key;
-
+    private LoginStepDef loginStepDef;
+    private Dashboard dashboard;
+    private Projects projects;
+    private SetupDashboard setupDashboard;
+    private FromScenarios fromScenarios;
+    private Dimensions dimensions;
 
     public StartedProjectStepDef(ProjectStepDef projectStepDef, LoginStepDef loginStepDef) {
         this.projectStepDef = projectStepDef;
